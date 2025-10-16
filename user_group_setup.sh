d959c4b36d3b8919cb57942fc47e0cf051ed8a66
Gowtham G <151497872+gow-05@users.noreply.github.com>
2025-10-16 18:28:30 +0530

    user_group_setup.sh added

--- user_group_setup.sh
+++ user_group_setup.sh
@@ -1,4 +1,4 @@
 [ "$EUID" -ne 0 ] && echo "Run as root!" && exit 1
-getent group intern_team >/dev/null || groupadd intern_team
-id intern_user >/dev/null 2>&1 || useradd -m -g intern_team intern_user
+groupadd intern_team
+useradd -m -g intern_team intern_user
 echo "User and group setup completed!"
